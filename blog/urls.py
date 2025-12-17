b06efb3fad68758a08572454595fbabd93e472e5
renjana-prasad <renju.elango@gmail.com>
2025-12-17 18:09:37 -0500

    basic_bog_posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_home, name='blog_home'),
+    path('posts', views.collected_post, name='collected_post'),
+    path('posts/<slug:slug>', views.individual_post, name='individual_post'),
 ]
--- my_site/settings.py
+++ my_site/settings.py
@@ -119,4 +119,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
+    BASE_DIR / 'static'
 ]
